9b54591970c721318d152fe595523d0278318f93
Tony Gaetani <tony.gaetani@gmail.com>
2016-06-03 01:51:16 +0200

    this works

--- src/sandbox/core.clj
+++ src/sandbox/core.clj
@@ -15,8 +15,7 @@
 
 (defn get-start-ids [sessionid]
   (->>
-   (client/get (str URL_BASE "/start") {:follow_redirects true
-                                        :headers {"Session" sessionid}})
+   (client/get (str URL_BASE "/start") {:headers {"Session" sessionid}})
    :body
    json/read-str
    (map smooth-keys)
@@ -24,43 +23,28 @@
    :next))
 
 (defn get-response-for-id [id sessionid]
-  ;; (pprint (str URL_BASE "/" id {:headers {"Session" sessionid}}))
   (->>
-   (client/get (str URL_BASE "/" id) {:follow_redirects true
-                                      :headers {"Session" sessionid}})
+   (client/get (str URL_BASE "/" id) {:headers {"Session" sessionid}})
    :body
    json/read-str
    (map smooth-keys)
    (into {})))
 
-(defn -make-request [req-fn sessionid]
-  (println "making request...")
-  (try
-    [(req-fn sessionid) sessionid]
-    (catch Exception e
-      ;; (pprint e)
-      (let [new-sessionid (get-sessionid)]
-        [(make-request req-fn new-sessionid) new-sessionid]))))
-
-(defn -find-secrets [id sessionid]
-  (let [[response sessionid] (make-request (partial get-response-for-id id) sessionid)]
-    ;; (pprint response)
-    (if (contains? response :secret)
-      (:secret response)
-      (map #(-find-secrets % sessionid) (:next response)))))
-
 (defn find-secrets [id sessionid]
   (try
     (let [response (get-response-for-id id sessionid)]
-      ;; (pprint response)
       (if (contains? response :secret)
         (:secret response)
         (map #(find-secrets % sessionid) (into [] (flatten [(:next response)])))))
     (catch Exception e
+      ;; assume the exception was caused by an expired session id
       (find-secrets id (get-sessionid)))))
 
 (defn main []
   (let [sessionid (get-sessionid)
         start-ids (get-start-ids sessionid)]
-    ;; (pprint start-ids)
-    (map #(find-secrets % sessionid) start-ids)))
+    (->>
+     start-ids
+     (map #(find-secrets % sessionid))
+     flatten
+     (filter not-empty))))
