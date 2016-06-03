2734c3184ebb7eeb49da45484cd9d21cb0e920b1
Tony Gaetani <tony.gaetani@gmail.com>
2016-06-03 17:45:17 +0200

    rename main, privatize fns/defs, main prints and retuns nil

--- src/sandbox/core.clj
+++ src/sandbox/core.clj
@@ -2,19 +2,19 @@
   (:require [clj-http.client :as client]
             [clojure.data.json :as json]))
 
-(defonce URL_BASE "http://challenge.shopcurbside.com")
+(defonce ^:private URL_BASE "http://challenge.shopcurbside.com")
 
-(defn get-sessionid []
+(defn- get-sessionid []
   (->> (client/get (str URL_BASE "/get-session"))
        :body))
 
-(defn lowercase-keywordize-keys [e]
+(defn- lowercase-keywordize-keys [e]
   {(->> (key e) .toLowerCase keyword) (val e)})
 
-(defn flatten-next-response [response]
+(defn- flatten-next-response [response]
   (into [] (flatten [(:next response)])))
 
-(defn get-start-ids [sessionid]
+(defn- get-start-ids [sessionid]
   (->> (client/get (str URL_BASE "/start") {:headers {"Session" sessionid}})
        :body
        json/read-str
@@ -22,14 +22,14 @@
        (into {})
        :next))
 
-(defn get-response-for-id [id sessionid]
+(defn- get-response-for-id [id sessionid]
   (->> (client/get (str URL_BASE "/" id) {:headers {"Session" sessionid}})
        :body
        json/read-str
        (map lowercase-keywordize-keys)
        (into {})))
 
-(defn find-secrets [id sessionid]
+(defn- find-secrets [id sessionid]
   (try
     (let [response (get-response-for-id id sessionid)]
       (if (contains? response :secret)
@@ -41,10 +41,11 @@
       ;; assume the exception was caused by an expired session id
       (find-secrets id (get-sessionid)))))
 
-(defn main []
+(defn challenge []
   (let [sessionid (get-sessionid)
         start-ids (get-start-ids sessionid)]
     (->> start-ids
          (map #(find-secrets % sessionid))
          flatten
-         (filter not-empty))))
+         (filter not-empty)
+         println)))
